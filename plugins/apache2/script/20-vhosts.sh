3666fcbaf6c1b2c163dae150911dbaea01916fa5
Tomáš Pecsérke <tomas@pecserke.eu>
2013-01-26 14:55:01 +0100

    create dir first; fixed order

--- plugins/apache2/script/20-vhosts.sh
+++ plugins/apache2/script/20-vhosts.sh
@@ -2,9 +2,12 @@
 
 baseDirectory=$(dirname $0)
 
-# iNotify tools
-apt-get install -y incron
-echo -e "\nroot" >> /etc/incron.allow
+mkdir -p /usr/share/lamp/
+cp $baseDirectory/../bin/* /usr/share/lamp/
+ln -s create_vhost.sh /usr/share/lamp/vhost_IN_CREATE
+ln -s create_vhost.sh /usr/share/lamp/vhost_IN_MOVED_TO
+ln -s delete_vhost.sh /usr/share/lamp/vhost_IN_DELETE
+ln -s delete_vhost.sh /usr/share/lamp/vhost_IN_MOVED_FROM
 
 # DNS
 apt-get install -y bind9 dnsutils
@@ -13,10 +16,8 @@ apt-get install -y bind9 dnsutils
 echo -e "\nnameserver 127.0.0.1" >> /etc/resolvconf/resolv.conf.d/head
 resolvconf -u
 
-cp $baseDirectory/../bin/* /usr/share/lamp/
-ln -s create_vhost.sh /usr/share/lamp/vhost_IN_CREATE
-ln -s create_vhost.sh /usr/share/lamp/vhost_IN_MOVED_TO
-ln -s delete_vhost.sh /usr/share/lamp/vhost_IN_DELETE
-ln -s delete_vhost.sh /usr/share/lamp/vhost_IN_MOVED_FROM
+# incron
+apt-get install -y incron
+echo -e "\nroot" >> /etc/incron.allow
 incrontab $baseDirectory/../config/incrontab
 service incrontab reload
