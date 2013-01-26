1cae92e5c3d0cffc49336da35e6e6a171c903323
Tomáš Pecsérke <tomas@pecserke.eu>
2013-01-26 21:11:42 +0100

    branch independent

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 wget -O lamp.tar.gz https://github.com/tomas-pecserke/lamp-install-script/archive/master.tar.gz
-tar -xzf lamp.tar.gz
+tar -xzf lamp.tar.gz -D lamp-install-script
 rm lamp.tar.gz
-lamp-install-script-master/install.sh
-rm -rf lamp-install-script-master
+mv lamp-install-script-* lamp-install-script
+lamp-install-script/install.sh
+rm -rf lamp-install-script
