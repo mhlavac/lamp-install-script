c05f21c6fea17233b21b19cb884eb86fc4b30537
Martin Hlavac <info@mhlavac.net>
2013-05-27 13:43:28 +0200

    Added nodejs, yo, grunt-cli and bower install

---
+++ plugins/nodejs/script/00-install.sh
@@ -0,0 +1,7 @@
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install nodejs
+
+npm install -g yo grunt-cli bower
+npm install grunt-contrib-sass
+npm install grunt-contrib-coffee
