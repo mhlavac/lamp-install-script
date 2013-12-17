41b472e9f6486a6d7df2962dec14634fc1836e72
mhlavac <info@mhlavac.net>
2013-12-17 20:13:59 +0100

    Postgres fix

--- plugins/postgresql/script/00-install.sh
+++ plugins/postgresql/script/00-install.sh
@@ -16,7 +16,7 @@ baseDirectory=$(dirname $0)
 echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 apt-get update
-apt-get install postgresql-9.3 phppgadmins
+apt-get install -y postgresql-9.3 phppgadmin
 echo "alter user postgres password 'lamp';" | psql
 
 # Only for workspace installation
